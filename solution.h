b0d50426c51419f37463bb3cf15c64c3a7154805
Denis Ramzaev <den.ramzaev@gmail.com>
2025-12-01 12:21:10 +0300

    feat: implement Component class with Peng-Robinson EOS support
    Add Component class with properties needed for thermodynamic calculations.

--- solution.h
+++ solution.h
@@ -10,16 +10,35 @@ namespace sol
 		GAS
 	};
 
-	//TODO: Component
 	class Component
 	{
+	public:
+		Component(
+			const std::string& name,
+			double critical_temperature,
+			double critical_pressure,
+			double accentric_factor,
+			double molar_mass
+		);
+
+		void		setName(const std::string& name);
+		void		setCriticalTemperature(double critical_temperature);
+		void		setCriticalPressure(double critical_pressure);
+		void		setAccentricFactor(double accentric_factor);
+		void		setMolarMass(double molar_mass);
+
+		std::string getName() const;
+		double		getCriticalTemperature() const;
+		double		getCriticalPressure() const;
+		double		getAccentricFactor() const;
+		double		getMolarMass() const;
 	private:
 		std::string name_;
 
-		double critical_temperature_;
-		double pressure_;
-		double accentric_factor_;
-		double molar_;
+		double		critical_temperature_;
+		double		critical_pressure_;
+		double		accentric_factor_;
+		double		molar_mass_;
 	};
 
 	//TODO: Concentrations
