5b8ed68e77a7599d9ad9ba7b8577a8886731a735
Denis Ramzaev <den.ramzaev@gmail.com>
2025-12-15 22:05:14 +0300

    feat: add methods for Solution class
    - get / set methods
    - calculate volume and calculate specific volume methods
    - also add callback field for calculating concnentration because it is dependent on pressures

--- solution.cpp
+++ solution.cpp
@@ -3,5 +3,142 @@
 
 namespace sol
 {
+	Solution::Solution(const std::vector<Component>& components, ConcentrationCallback callback, Correlation correlation, std::unique_ptr<eos::ICubicEOS> eos, const State& current_state)
+		: components_(components)
+		, concentations_(callback(current_state))
+		, concentration_callback_(callback)
+		, bip_(eos->calculateBIP(components, correlation, current_state.phase))
+		, eos_(std::move(eos))
+		, current_state_(current_state)
+	{
+	}
+
+	std::vector<Component> Solution::getComponents() const
+	{
+		return components_;
+	}
+
+	std::vector<double> Solution::getConcentrations() const
+	{
+		return concentations_;
+	}
+
+	Matrix<double> Solution::getBIP() const
+	{
+		return bip_;
+	}
+
+	State Solution::getState()
+	{
+		if (!current_state_.volume)
+		{
+			current_state_.volume = calculateVolume();
+		}
+		if (!current_state_.specific_volume)
+		{
+			current_state_.specific_volume = calculateSpecificVolume();
+		}
+
+		return current_state_;
+	}
+
+	void Solution::setComponents(const std::vector<Component> components)
+	{
+		components_ = components;
+	}
+
+	void Solution::setCallback(const ConcentrationCallback& callback)
+	{
+		concentration_callback_ = callback;
+	}
+
+	void Solution::setBip(Correlation correlation)
+	{
+		bip_ = eos_->calculateBIP(components_, correlation, current_state_.phase);
+	}
+
+	void Solution::setEOS(std::unique_ptr<eos::ICubicEOS> eos)
+	{
+		eos_ = std::move(eos);
+	}
+
+	void Solution::setState(const State& state)
+	{
+		current_state_ = state;
+		concentations_ = concentration_callback_(state);
+	}
+
+	double Solution::calculateVolume()
+	{
+		if (current_state_.volume)
+		{
+			return *current_state_.volume;
+		}
+
+		auto roots = eos_->calculateZFactor(components_, concentations_, bip_, current_state_);
+		auto z_factor = 0.0;
+
+		switch (current_state_.phase)
+		{
+		case sol::Phase::LIQUID:
+			z_factor = *std::min_element(roots.begin(), roots.begin());
+			break;
+		case sol::Phase::GAS:
+			z_factor = *std::max_element(roots.begin(), roots.begin());
+			break;
+		default:
+			break;
+		}
+
+		auto R = constants::universal_gas_constant;
+		auto t = current_state_.temperature;
+		auto p = current_state_.pressure;
+
+		current_state_.volume = (z_factor * R * t) / p;
+
+		return *current_state_.volume;
+	}
+
+	double Solution::calculateSpecificVolume()
+	{
+		if (current_state_.specific_volume)
+		{
+			return *current_state_.specific_volume;
+		}
+
+		if (!current_state_.volume)
+		{
+			auto roots = eos_->calculateZFactor(components_, concentations_, bip_, current_state_);
+			auto z_factor = 0.0;
+
+			switch (current_state_.phase)
+			{
+			case sol::Phase::LIQUID:
+				z_factor = *std::min_element(roots.begin(), roots.begin());
+				break;
+			case sol::Phase::GAS:
+				z_factor = *std::max_element(roots.begin(), roots.begin());
+				break;
+			default:
+				break;
+			}
+
+			auto R = constants::universal_gas_constant;
+			auto t = current_state_.temperature;
+			auto p = current_state_.pressure;
+
+			current_state_.volume = (z_factor * R * t) / p;
+		}
+
+		double weight = 0;
+		for (size_t i = 0; i < components_.size(); i++)
+		{
+			weight += concentations_[i] * components_[i].molar_mass;
+		}
+
+		current_state_.specific_volume = *current_state_.volume / weight;
+
+		return *current_state_.specific_volume;
+	}
 
 }
--- solution.h
+++ solution.h
@@ -5,26 +5,45 @@
 #include <string>
 #include <memory>
 #include <optional>
+#include <functional>
 
 namespace sol
 {
+	using ConcentrationCallback = std::function<std::vector<double>(State)>;
+
 	class Solution
 	{
 		Solution(
 			const std::vector<Component>& components,
-			const std::vector<double>& concentrations,
+			ConcentrationCallback callback,
 			Correlation correlation,
 			std::unique_ptr<eos::ICubicEOS> eos,
 			const State& current_state
 			);
 
+		std::vector<Component>			getComponents() const;
+		std::vector<double>				getConcentrations() const;
+		Matrix<double>					getBIP() const;
+		State							getState();
+
+		void							setComponents(const std::vector<Component> components);
+		void							setCallback(const ConcentrationCallback& callback);
+		void							setBip(Correlation correlation);
+		void							setEOS(std::unique_ptr<eos::ICubicEOS> eos);
+		void							setState(const State& state);
+
+		double							calculateVolume();
+		double							calculateSpecificVolume();
+
 	private:
 		std::vector<Component>			components_;
 		std::vector<double>				concentations_;
+		ConcentrationCallback			concentration_callback_;
 		Matrix<double>					bip_;
 
 		std::unique_ptr<eos::ICubicEOS>	eos_;
 
 		State							current_state_;
 	};
+
 }
