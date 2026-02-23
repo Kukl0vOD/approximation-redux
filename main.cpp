ab37912e9c67377059468c3eb8ba74921914251d
Denis Ramzaev <den.ramzaev@gmail.com>
2026-02-23 18:20:00 +0300

    -

--- main.cpp
+++ main.cpp
@@ -20,15 +20,20 @@ int main()
 
 	Plotter plt(controller);
 
-	//plt.plotComparativeMolarVolumes(1.0, 4.97, 0.01, { 1.2,3.0,4.8 }, { 0, 1 });
-	//plt.plotComparativeMolarVolumes(1.0, 13.8, 0.01, { 1.67,7.74,13.8 }, { 2, 3 });
-	//plt.plotComparativeMolarVolumes(1.0, 26.0, 0.01, { 1.1,12.0,26.8 }, { 4, 5 });
-	//plt.plotComparativeMolarVolumes(4.1, 38.6, 0.01, { 5.92,22.26,38.6 }, { 6, 7 });
+	plt.plotComparativeMolarVolumes(1.0, 4.97, 0.01, { 1.2,3.0,4.8 }, { 0, 1 });
+	plt.plotComparativeMolarVolumes(1.0, 13.8, 0.01, { 1.67,7.74,13.8 }, { 2, 3 });
+	plt.plotComparativeMolarVolumes(1.0, 26.0, 0.01, { 1.1,12.0,26.8 }, { 4, 5 });
+	plt.plotComparativeMolarVolumes(4.1, 38.6, 0.01, { 5.92,22.26,38.6 }, { 6, 7 });
 
-	plt.plotComparativeKValues(1.0, 4.97, 0.01, { 1.2,3.0,4.8 }, { 0, 1 });
-	plt.plotComparativeKValues(1.0, 13.8, 0.01, { 1.67,7.74,13.8 }, { 2, 3 });
-	plt.plotComparativeKValues(1.0, 26.0, 0.01, { 1.1,12.0,26.8 }, { 4, 5 });
-	plt.plotComparativeKValues(4.1, 38.6, 0.01, { 5.92,22.26,38.6 }, { 6, 7 });
+	plt.plotComparativeSpecificVolumes(1.0, 4.97, 0.01, { 1.2,3.0,4.8 }, { 0, 1 });
+	plt.plotComparativeSpecificVolumes(1.0, 13.8, 0.01, { 1.67,7.74,13.8 }, { 2, 3 });
+	plt.plotComparativeSpecificVolumes(1.0, 26.0, 0.01, { 1.1,12.0,26.8 }, { 4, 5 });
+	plt.plotComparativeSpecificVolumes(4.1, 38.6, 0.01, { 5.92,22.26,38.6 }, { 6, 7 });
+
+	//plt.plotComparativeKValues(1.0, 4.97, 0.01, { 1.2,3.0,4.8 }, { 0, 1 });
+	//plt.plotComparativeKValues(1.0, 13.8, 0.01, { 1.67,7.74,13.8 }, { 2, 3 });
+	//plt.plotComparativeKValues(1.0, 26.0, 0.01, { 1.1,12.0,26.8 }, { 4, 5 });
+	//plt.plotComparativeKValues(4.1, 38.6, 0.01, { 5.92,22.26,38.6 }, { 6, 7 });
 
 	return 0;
 }
